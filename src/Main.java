6b028df165e06e20144256de459f605901c01d1c
xhuriken <celestin@honvault.Com>
2025-01-20 20:39:46 +0100

    Add TODO per story in main for better organization

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //STORY 1:
+            //Faire un menu pour regrouper plusieurs fonctionalité (chacune dans une fonction quoi)
+        //STORY 2:
+            //Faire la fonction display_rules() -> Accecible depuis le menu dcp quoi enfaite tukapt
+        //STORY 3:
+            //GROSSE FONCTION PLAY()
+            //Elle appelleras plusieur sous fonction comme celle de la story 3 'initialize_game()'
+            //Le jeu attribue un pseudo aux deux joueurs et affiche le plateau de jeux.
+            // De manière aléatoire un joueur est désigné pour commencer à jouer.
+        //STORY 4:
+            //On réfléchiras à la meilleure optimisation de play, mais on va essayer de faire en plein de petit bout de code different
+            //DONT CELUI DE LA STORY 4 LOL VOUS COMMENCEZ A CAPTER OU KOA LES BOYZ (je deviens fous je parle tout seul dans des commentaire donnez moi de l'argent)
+            //Move(player) -> fonction pour déplacer le joueur donné OUAIIII
+        //STORY 5:
+            //canMove(player) -> Verifier si le joueur peu encore bouger
+            //canMove seras ensuite appeler dans une fonction Win() pour savoir si c'est gagné quoi fait pas genre ta pas compris c'est ecrit dans le nom zbi
+        //STORY 6:
+            // Ca commence a être les ajout qui serve a rien donc j'ai la flm allez bisous jvous passe le salam quoi
     }
 }
