ba36dcef4f6946bd7d275baffbf22f5be4f92eb6
Clément Le Goffic <clementlegoffic@gmail.com>
2025-01-21 10:48:06 +0100

    initial commit

--- src/Players.java
+++ src/Players.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Players {
 
     String name;
@@ -17,4 +19,22 @@ public class Players {
     public short GetY(){
         return y;
     }
+
+    public void Mouve(short y , short x, Players players){ 
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the direction you want to move: ");
+        String direction = sc.nextLine();
+
+        // if (direction == "top") {
+        //     y += ;
+        // } else if (direction == "left") {
+        //     x += ;
+        // } else if (direction == "right") {
+        //     x += right;
+        // } else if (direction == "bottom") {
+        //     y += bottom;
+        // }
+    
+}
 }
