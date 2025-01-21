47a0ff2ee84463e363f0ae896b44c3109774d593
Clément Le Goffic <clementlegoffic@gmail.com>
2025-01-21 11:15:41 +0100

    add function Mouve in Player

--- src/Players.java
+++ src/Players.java
@@ -20,21 +20,61 @@ public class Players {
         return y;
     }
 
-    public void Mouve(short y , short x, Players players){ 
+    //Function to move the player
+    /**
+     * 
+     * @param y
+     * @param x
+     * @param players
+     * 
+     * Function that manages the player's movements 
+     */
 
+    public void Mouve(short y , short x, Players players , char[][] grid){ 
+        String input;
+        boolean Choice = true;
+        //Request the player to enter a letter
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the direction you want to move: ");
-        String direction = sc.nextLine();
+        System.out.println("Veuillez entrer 'z' ou 'q' ou 's' ou 'd' :");
+        input = sc.nextLine().toLowerCase();
+        //As long as Choice is true, we continue 
+        while (Choice == true) {
+        //check if the input is valid
+        if (input.equals("z") || input.equals("q") || input.equals("s") || input.equals("d")) {
 
-        // if (direction == "top") {
-        //     y += ;
-        // } else if (direction == "left") {
-        //     x += ;
-        // } else if (direction == "right") {
-        //     x += right;
-        // } else if (direction == "bottom") {
-        //     y += bottom;
-        // }
-    
+            if (input.equals("z")) {
+                if (grid[x][y - 1] == '.') {
+                    y -= 1;
+                }
+                else {
+                    System.out.println("Vous ne pouvez pas aller ici");
+                }
+            } else if (input.equals("q")) {
+                if (grid[x - 1][y] == '.') {
+                    x -= 1;
+                }
+                else {
+                    System.out.println("Vous ne pouvez pas aller ici");
+                }
+            } else if (input.equals("s")) {
+                if (grid[x][y + 1] == '.') {
+                    y += 1;
+                }
+                else {
+                    System.out.println("Vous ne pouvez pas aller ici");
+                }
+            } else if (input.equals("d")) {
+                if (grid[x + 1][y] == '.') {
+                    x += 1;
+                }
+                else {
+                    System.out.println("Vous ne pouvez pas aller ici");
+                }
+            }
+
+        } else {
+            System.out.println("Veuillez entrer une lettre valide");
+        } 
+        }
 }
 }
