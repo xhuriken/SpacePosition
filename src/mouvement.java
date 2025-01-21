de719ba93252c697c57bdf247a40a7b21a1026f2
Clément Le Goffic <clementlegoffic@gmail.com>
2025-01-21 10:05:26 +0100

    add file mouvement + start founction mouv

---
+++ src/mouvement.java
@@ -0,0 +1,16 @@
+public class mouvement {
+    
+    short top;
+    short left;
+    short right;
+    short bottom;
+
+    mouvement(short top, short left, short right, short bottom) {
+        this.top = top;
+        this.left = left;
+        this.right = right;
+        this.bottom = bottom;
+
+        
+    }
+}
