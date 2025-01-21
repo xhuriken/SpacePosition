ec861c3817bdba21eb02228ff0c5bd09bf6bf58b
Mateis B <mateisbourlet9@gmail.com>
2025-01-21 12:19:14 +0100

    init Save_data

--- src/Save_data.java
+++ src/Save_data.java
@@ -5,10 +5,9 @@ public class Save_data{
 
     public void initdata(){
         char[][] grid = new char[10][11];
-        Players[] players = new Players[2];
+        
 
-        players[0] = new Players("didi", (short) 5, (short) 6);
-        players[1] = new Players("dede", (short) 6, (short) 6);
+
 
         grid = new char[10][11];
         for (int i = 0; i < grid.length; i++) {
@@ -19,6 +18,12 @@ public class Save_data{
         grid[0][0] = 'X';
         grid[1][0] = 'X';
         
+        // Init 2 players fictifs
+        Players[] players = new Players[2];
+
+        players[0] = new Players("didi", (short) 5, (short) 6);
+        players[1] = new Players("dede", (short) 6, (short) 6);
+
         System.out.println("Joueur 1 : " + players[0].GetName() + " X : " + players[0].GetX() + " Y : " + players[0].GetY());
         System.out.println("Joueur 2 : " + players[1].GetName() + " X : " + players[1].GetX() + " Y : " + players[1].GetY());
     }
