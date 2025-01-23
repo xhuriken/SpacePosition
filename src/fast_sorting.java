81ca278c5a89298b24051412beb6b780da65e293
Mateis B <mateisbourlet9@gmail.com>
2025-01-23 12:44:24 +0100

    add recovery score to sort

--- src/fast_sorting.java
+++ src/fast_sorting.java
@@ -2,6 +2,21 @@ public class fast_sorting {
 
     int Arr[] = {5, 9, 4, 6, 5, 3};
 
+
+
+    public void Datascore(){
+        // Nouvelle instance de la classe Save_score
+        Save_score save_score = new Save_score();
+        // Permet de récupérer les scores sous forme de tableau 2D : [Nom, Score]
+        String[][] scoresArray = save_score.ScorestoSimpleArray();
+    
+        // Pour afficher le tableau
+        for (String[] entry : scoresArray) {
+            System.out.println( entry[0] +" : "+entry[1]);
+        }
+    }
+    
+
     public void quickSort(int arr[], int begin, int end) {
         if (begin < end) {
             int partitionIndex = partition(arr, begin, end);
@@ -30,5 +45,9 @@ public class fast_sorting {
         arr[end] = swapTemp;
 
         return i+1;
+    }
+    public static void main(String[] args) {
+        fast_sorting sorter = new fast_sorting();
+        sorter.Datascore();
     }
 }
